69efa3ca5c8e0fe62861f39141d6151698ce06d0
corvocorav <corvojaeger@gmail.com>
2021-04-17 00:21:41 +0100

    added openings page

---
+++ functions.js
@@ -0,0 +1,48 @@
+ 
+function play1()
+{
+
+
+    window.open ( "https://www.youtube.com/watch?v=AgBUP8TJqV8", "_blank");
+}
+
+function play2()
+{
+
+
+    window.open ( "https://www.youtube.com/watch?v=EO8EadQNIXU", "_blank");
+}
+
+function play3()
+{
+
+
+    window.open ( "https://www.youtube.com/watch?v=LKP-vZvjbh8", "_blank");
+}
+
+function play4()
+{
+
+
+    window.open ( "https://www.youtube.com/watch?v=r1XE8ON8fos", "_blank");
+}
+
+function play5()
+{
+
+
+    window.open ( "https://www.youtube.com/watch?v=0dK7JgKivQM", "_blank");
+}
+
+function play6()
+{
+
+
+    window.open ( "https://www.youtube.com/watch?v=mqJ55FFoiCY", "_blank");
+}
+
+
+function openings_load()
+{
+    window.location = "ops_index.html";
+}
